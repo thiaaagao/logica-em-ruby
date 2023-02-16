c6c9f652a1dd145aeec51d06571595002376f675
thiaaagao <rdes.thiago@gmail.com>
2023-02-16 17:42:16 -0300

    Calc imc using Ruby

---
+++ imc.rb
@@ -0,0 +1,9 @@
+puts('Informe a sua altura:
+    Ex: 1.75')
+altura = gets().chomp.to_f
+
+puts('Informe o seu peso em KG: 
+    Ex: 60.0')
+peso = gets().chomp.to_f
+
+puts("Seu peso é #{peso} Kg e sua altura é #{altura}.")
